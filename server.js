9b97ec3ca756c2b4d013d3a24d77acf9b8562f61
Karuppannan <karuppannan.rm.06@gmail.com>
2024-05-28 17:37:37 +0530

    Initial commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const pool = mysql.createPool({
   password: 'Harihara11!+'
 });
 
-app.get('/api/state', (req, res) => {
+app.get('/state', (req, res) => {
   const query = 'SELECT * FROM state';
   pool.query(query, (err, results) => {
     if (err) {
